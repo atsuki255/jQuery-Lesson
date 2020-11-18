677b4bcf47581fc6ed6bbe85f67c69540120b82c
atsuki225 <atsuki443@gmail.com>
2020-11-18 23:16:33 +0900

    [add]fadein

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ $(function(){
     $('ul').children().css('color','red');
   });
 });
+
+
+var window_h = $(window).height();
+$("#wh span").text(window_h);
+//スクロールイベント
+$(window).on("scroll", function() {
+  var scroll_top = $(window).scrollTop();
+  $("#scroll span").text(scroll_top);
+
+  $(".box").each(function() {
+    var elem_pos = $(this).offset().top;
+    $(this).find(".box_pos span").text(Math.floor(elem_pos));
+  　
+    //どのタイミングでフェードインさせるか
+    if (scroll_top >= elem_pos - window_h+200) {
+      $(this).addClass("fadein");
+    } else {
+      $(this).removeClass("fadein");
+    }
+  });
+});
